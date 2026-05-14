f3973b0abd1cb8b168291c57feaff7e4907ff466
Ali Azouz <aliazouz.808@gmail.com>
2026-05-14 19:57:43 +0300

    Add Async Wrapper and modify admin controller to use asyncWrapper middleware and add appError class

--- BackEnd/controllers/adminController.js
+++ BackEnd/controllers/adminController.js
@@ -1,34 +1,42 @@
 import User from "../models/User.js";
 import Doctor from "../models/Doctor.js";
+import asyncWrapper from "../middlewares/asyncWrapper.js";
+import httpStatus from "../utils/httpStatus.js";
+
 //All the admin can do for now is banning the users instead of deleting them.
-export const getAllUsers = async (req, res) => {
-  try {
+export const getAllUsers = asyncWrapper(
+  async (req, res, next) => {
     const users = await User.find().select("-password");
-    return res
-      .status(200)
-      .json({ success: true, count: users.length, data: { users } });
-  } catch (error) {
-    return res.status(500).json({ success: false, message: error.message });
+    if (!users) {
+      return next(appError.create("Something went wrong", 500, httpStatus.ERROR))
+    }
+    return res.status(200)
+      .json({ success: httpStatus.SUCCESS, count: users.length, data: { users } });
   }
-};
-export const toggleUserStatus = async (req, res) => {
-  try {
+)
+export const toggleUserStatus = asyncWrapper(
+  async (req, res, next) => {
+
     const { id } = req.params;
+    if (!id) {
+      return next(appError.create("No Id Provided", 404, httpStatus.FAIL))
+    }
     const user = await User.findById(id);
     if (!user) {
-      return res.status(404).json({ success: false, message: "User not found" });
+      return next(appError.create("User not found", 404, httpStatus.FAIL))
     }
     const updatedUser = await User.findByIdAndUpdate(
       id,
-      { $set: { isActive: !user.isActive } }, 
+      { $set: { isActive: !user.isActive } },
       { returnDocument: 'after', runValidators: false }
     );
+    if (!updatedUser) {
+      return next(appError.create("Something went wrong", 500, httpStatus.ERROR))
+    }
     return res.status(200).json({
-      success: true,
+      success: httpStatus.SUCCESS,
       message: `User status changed to: ${updatedUser.isActive ? "Active" : "Suspended"}`,
       data: { isActive: updatedUser.isActive }
     });
-  } catch (error) {
-    return res.status(500).json({ success: false, message: error.message });
   }
-};
+)
