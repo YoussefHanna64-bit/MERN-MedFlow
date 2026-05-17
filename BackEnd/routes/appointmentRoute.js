329c1d10ca5f76867eaceedec2f2799486547e26
Ali Azouz <aliazouz.808@gmail.com>
2026-05-17 11:43:01 +0300

    - Modify Appointment Controller - Add Route Protecting

--- BackEnd/routes/appointmentRoute.js
+++ BackEnd/routes/appointmentRoute.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import verifyToken from "../middlewares/verifyToken.js";
+import authorize from "../middlewares/authorize.js";
 import {
     bookAppointment,
     getPatientAppointments,
@@ -7,9 +9,9 @@ import {
 } from '../controllers/appointementController.js'
 
 const router = express.Router();
-router.post("/book", bookAppointment)
-    .get("/patient/my-bookings", getPatientAppointments)
-    .get("/doctor/my-bookings", getDoctorAppointements)
-    .patch("/cancel/:id", cancelAppointment)
+router.post("/book", verifyToken, authorize("patient"), bookAppointment)
+    .get("/patient/my-bookings", verifyToken, authorize("patient"), getPatientAppointments)
+    .get("/doctor/my-bookings", verifyToken, authorize("doctor"), getDoctorAppointements)
+    .patch("/cancel/:id", verifyToken, authorize("patient", "doctor"), cancelAppointment)
 
 export default router;
