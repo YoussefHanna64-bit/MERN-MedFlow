2e04ae48c3f14db05d7699f1105b6fb70295b690
YoussefHanna64-bit <JCBIOX200@GMAIL.COM>
2026-05-23 11:48:06 +0300

    Feat: add routing with MainLayout and AuthLayout

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,35 @@
+import { lazy } from "react";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
 import { Provider } from "react-redux";
 import "./App.css";
-import DoctorAppointments from "./pages/DoctorAppointments";
 import { doctorAppointmentsStore } from "./redux/store/store";
+import MainLayout from "./layouts/MainLayout";
+import AuthLayout from "./layouts/AuthLayout";
+const DoctorAppointments = lazy(() => import("./pages/DoctorAppointments"));
+
+const routerCofig = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <Navigate to="/home" replace /> },
+      {
+        path: "home",
+        element: <DoctorAppointments />,
+      },
+    ],
+  },
+  {
+    element: <AuthLayout />,
+    children: [],
+  },
+  { path: "*", element: <Navigate to="/home" replace /> },
+]);
 
 function App() {
   return (
     <div className="h-full">
       <Provider store={doctorAppointmentsStore}>
-        <DoctorAppointments />
+        <RouterProvider router={routerCofig} />
       </Provider>
     </div>
   );
